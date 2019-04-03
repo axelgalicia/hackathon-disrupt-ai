3a256eb6b471bddea1b358ea3a10d4ac8e26d9cf
Jifeng Jiang <jjiang@loyalty.com>
2019-04-03 16:09:21 -0400

    Added axios to send request, added local and remote image placeholders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,94 @@
 import React, { useCallback, useState } from 'react';
 import {useDropzone} from 'react-dropzone'
+import axios from 'axios';
 import './App.scss';
 
+const remoteUrl = 'https://jsonplaceholder.typicode.com/users';
+
 const App = () => {
+  const [fileUpLoading, setFileUpLoading] = useState(false);
   const [localImage, setLocalImage] = useState(null);
+  const [remoteImage, setRemoteImage] = useState(null);
+  const [showUploadButton, setShowUploadButton] = useState(false);
   const onDrop = useCallback(acceptedFiles => {
     const reader = new FileReader()
     reader.onabort = () => console.log('file reading was aborted')
     reader.onerror = () => console.log('file reading has failed')
     reader.onload = () => {
       const base64Encoded = reader.result
-      setLocalImage(base64Encoded)
+      base64Encoded && setLocalImage(base64Encoded)
+      setShowUploadButton(true);
     }
     acceptedFiles.forEach(file => reader.readAsDataURL(file))
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  const handleUpload = (image) => {
+    setFileUpLoading(true);
+    try{
+      axios.post(remoteUrl, { image })
+      .then(res => {
+        setShowUploadButton(false);
+        setFileUpLoading(false);
+        const imgData = res && res.data;
+        const imgSrc = imgData && imgData.image;
+        imgData && setRemoteImage(imgSrc);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    } catch(e) {
+      console.log(e);
+    }
+  }
   return (
-    <div {...getRootProps()} className="App--local">
-      <input {...getInputProps()} accept=".jpg, .jpeg, .png, .gif, .ico, .tiff"/>
+    <>
+      <div className="app">
+        <div className="app--local">
+          <div {...getRootProps()}>
+            <input 
+            {...getInputProps()} 
+            accept=".jpg, .jpeg, .png, .gif, .ico, .tiff"
+            className="app--local__input"/>
+            { 
+              localImage ?
+              <>
+                <img src={localImage} alt="local" className="app--local__image"/>
+                <p>Click to select an image</p>
+              </>
+              :
+              <div className="app--local__wrapper">
+                <p>Drag 'n' drop an image here</p>
+              </div>
+            }
+          </div>
+          {
+            showUploadButton && 
+            <>
+              <button onClick={() => handleUpload(localImage)} className="App--local__button">Upload</button>      
+            </>
+          }
+        </div>
+        <div className="app--remote">
+          {
+            remoteImage ?
+            <>
+              <img src={remoteImage} alt="remote" className="app--remote__image"/>
+              <p>Converted image</p>
+            </> :
+            <div className="app--remote__placeholder">
+              <p>Placeholder for converted image</p>
+            </div>
+          }
+        </div>
+      </div>
       {
-        isDragActive ?
-          <p>Drop your image here</p> :
-          <p>Drag 'n' drop an image here, or click to select an image</p>
+        fileUpLoading &&
+        <div className="loading">
+          Uploading image...
+        </div>
       }
-      {
-        localImage && 
-        <img src={localImage} alt="local" className="App--local__image"/>
-      }
-    </div>
+    </>
   )
 }
-
 
 export default App;
