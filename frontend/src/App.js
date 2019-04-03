4450285832ec1652d8dfef859e161052a9f5a1ff
Jifeng Jiang <jjiang@loyalty.com>
2019-04-03 14:22:34 -0400

    Show image preview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import {useDropzone} from 'react-dropzone'
 import './App.scss';
 
 const App = () => {
+  const [localImage, setLocalImage] = useState(null);
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
     const reader = new FileReader()
-
     reader.onabort = () => console.log('file reading was aborted')
     reader.onerror = () => console.log('file reading has failed')
     reader.onload = () => {
-      // Do whatever you want with the file contents
       const base64Encoded = reader.result
-     console.log(base64Encoded)
+      setLocalImage(base64Encoded)
     }
-
-    acceptedFiles.forEach(file => reader.readAsDataURL(file));
-
+    acceptedFiles.forEach(file => reader.readAsDataURL(file))
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
+    <div {...getRootProps()} className="App--local">
+      <input {...getInputProps()} accept=".jpg, .jpeg, .png, .gif, .ico, .tiff"/>
       {
         isDragActive ?
           <p>Drop your image here</p> :
           <p>Drag 'n' drop an image here, or click to select an image</p>
+      }
+      {
+        localImage && 
+        <img src={localImage} alt="local" className="App--local__image"/>
       }
     </div>
   )
